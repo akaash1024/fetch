1ef3a8d23af07dafc0ef5d0c555d565bfdb43940
akashkewat99 <akashkewat99@gmail.com>
2024-10-10 13:10:37 +0530

    updated

---
+++ CRUD - todo/index.js
@@ -0,0 +1,142 @@
+const paginationContainer = document.querySelector('.pagination')
+const todosContainer = document.querySelector(".todos")
+
+let id=10; //taking by default for learning purpose
+let currentPage = 1;
+const perPage = 5;
+
+const createPagination = ((totalPages)=>{
+  paginationContainer.innerHTML = "";
+
+  for (let i = 1; i <= totalPages; i++) {
+    const PageButton = document.createElement('button');
+    PageButton.innerText=i;
+
+    PageButton.addEventListener("click",()=>{
+      currentPage = i;
+      fetchTodos()
+    })
+
+    paginationContainer.appendChild(PageButton);
+
+    
+  }
+})
+
+
+
+const displayTodos = (todos) => {
+    todosContainer.innerHTML = "";
+    todos.forEach(element => {
+
+    const div = document.createElement("div")
+    div.classList.add('todo-item');
+
+    const todoItem = document.createElement('p');
+    todoItem.innerHTML = element.text;
+
+    const subDiv = document.createElement('div');
+
+    const status = document.createElement('p');
+    status.innerText = element.status ? "Done" : "To be done"
+
+    const toggleButton = document.createElement('button')
+    toggleButton.classList.add('toggle-btn');
+    toggleButton.textContent="Toggle Status"
+
+    toggleButton.addEventListener("click",async()=>{
+      await fetch(`http://localhost:3000/todos/${element.id}`,{
+        method:"PATCH",
+        body:JSON.stringify ({
+          status: !element.status
+        }),
+        headers:{
+          "Content-Type": "application/json",
+        }
+      })
+      fetchTodos()
+    })
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-btn");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", async ()=>{
+      await fetch(`http://localhost:3000/todos/${element.id}`,{method: "DELETE"});
+      fetchTodos()
+    })
+
+    subDiv.append(status,toggleButton,deleteButton)
+
+    div.append(todoItem,subDiv)
+
+    todosContainer.appendChild(div)
+    });
+};
+
+
+
+const input = document.querySelector("input")
+const button = document.getElementById("add-todo").addEventListener("click",addTodo)
+
+async function addTodo(){
+    const todo = input.value;
+
+    if (!todo) {
+        show.innerHTML = `<p>Please enter a To-Do!</p>`;
+        return;
+    }
+    try{
+        const res = await fetch("http://localhost:3000/todos",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(
+                {
+                    "id":String(id),
+                    "text":todo,
+                    "status":false
+                },  
+            )
+        })
+        id++;
+        input.value = "";
+        fetchTodos()
+        
+    } catch(err){
+        console.log("Error",err);
+    }
+    
+}
+
+
+let filterButton = document.querySelector('#filterButton')
+let sortButton = document.querySelector('#sortButton')
+
+sortButton.addEventListener("click",()=>{
+  fetchTodos("isCompleted=false")
+});
+
+filterButton.addEventListener("click",()=>{
+  fetchTodos("_sort=text")
+});
+
+
+
+
+
+async function fetchTodos(filterQuery){
+  const apiURL = filterQuery ? `http://localhost:3000/todos?_page=${currentPage}&_per_page=${perPage}&${filterQuery}` : `http://localhost:3000/todos?_page=${currentPage}&_per_page=${perPage}`
+    try{
+        const res = await fetch(apiURL);
+        const data = await res.json()
+        console.log(data);
+        displayTodos(data.data)
+        createPagination(data.pages)
+    } catch(err){
+        console.log("Error",err);
+    }
+}
+fetchTodos()
+
+
--- fetch1/2/Submission/index.js
+++ fetch1/2/Submission/index.js
@@ -1,36 +1,35 @@
-const show = document.getElementById("show")
-const fetchUsers = document.getElementById("fetchUsers").addEventListener("click",fetchUsersData)
+const show = document.getElementById("show");
+const fetchUsers = document
+  .getElementById("fetchUsers")
+  .addEventListener("click", fetchUsersData);
 
 const distplayUserData = (data) => {
-    show.innerHTML="";
-    data.forEach((user) => {
-        
-        const div = document.createElement("div")
+  show.innerHTML = "";
+  data.forEach((user) => {
+    const div = document.createElement("div");
 
-        const email = document.createElement("h3")
-        email.innerText = user.email
+    const email = document.createElement("h3");
+    email.innerText = user.email;
 
-        const avatar = document.createElement("img")
-        avatar.src = user.avatar
+    const avatar = document.createElement("img");
+    avatar.src = user.avatar;
 
-        const fullName = document.createElement("h1");
-        fullName.innerText = `${user.first_name} ${user.last_name}`;
+    const fullName = document.createElement("h1");
+    fullName.innerText = `${user.first_name} ${user.last_name}`;
 
-        div.append(avatar,fullName,email)
-        show.append(div)
-    })
-}
+    div.append(avatar, fullName, email);
+    show.append(div);
+  });
+};
 
-
-
-async function fetchUsersData(){
-    try{
-        const res = await fetch("https://reqres.in/api/users?page=2");
-        const data = await res.json();
-        x=data["data"]
-        // console.log(x); 
-        distplayUserData(x)
-    } catch(err){
-        console.log("Error",err);
-    }
+async function fetchUsersData() {
+  try {
+    const res = await fetch("https://reqres.in/api/users?page=2");
+    const data = await res.json();
+    x = data["data"];
+    // console.log(x);
+    distplayUserData(x);
+  } catch (err) {
+    console.log("Error", err);
+  }
 }
